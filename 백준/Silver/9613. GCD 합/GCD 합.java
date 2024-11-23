a414678905a3d5cf77713ca747e19d4c49228109
yeo-li <81564503+yeo-li@users.noreply.github.com>
2024-11-24 00:33:17 +0900

    [Silver IV] Title: GCD 합, Time: 320 ms, Memory: 37600 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.java
+++ 백준/Silver/9613. GCD 합/GCD 합.java
@@ -1,30 +1,41 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int num = Integer.parseInt(br.readLine());
-		while (num-- > 0) {
-			List<Integer> list = new ArrayList<>();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		
+		for(int k = 0; k < t; k++) {
+			Long result = 0L;
+			List<Integer> numbers = new ArrayList<>();
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			long sum = 0;
-			int n = Integer.parseInt(st.nextToken());
-			for (int i = 0; i < n; i++) {
-				list.add(Integer.parseInt(st.nextToken()));
+			st.nextToken();
+			while(st.hasMoreTokens()) {
+				numbers.add(Integer.parseInt(st.nextToken()));
 			}
-			for (int i = 0; i < n; i++) {
-				for (int j = i + 1; j < n; j++) {
-					sum += gcd(list.get(i), list.get(j));
+			
+			for(int i = 0; i < numbers.size() -1; i++) {
+				for(int j = i+1; j < numbers.size(); j++) {
+					result += gcd(numbers.get(i), numbers.get(j));
 				}
 			}
-			System.out.println(sum);
+			
+			bw.write(result + "\n");
 		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
 	}
-
-	public static int gcd(int a, int b) {
-		if (a == 0) return b;
+	
+	private static int gcd(int a, int b) {
+		if(a == 0) return b;
 		return gcd(b % a, a);
 	}
+
 }

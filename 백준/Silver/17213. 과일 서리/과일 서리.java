2feb0e9d2707f7ff2195361ea40a4dc16cfa7216
yeo-li <81564503+yeo-li@users.noreply.github.com>
2024-11-25 22:25:29 +0900

    [Silver II] Title: 과일 서리, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/17213. 과일 서리/과일 서리.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		int[][] pascal = new int[N + M][N + M];
+		for(int i = 0; i < N + M; i++) {
+			for(int j = 0 ; j <= i; j++) {
+				if(i == 0 || j == 0) {
+					pascal[i][j] = 1;
+ 				} else {
+ 					pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+ 				}
+			}
+		}
+		
+		bw.write(String.valueOf(pascal[M - 1][M - N]));
+		
+		bw.close();
+		br.close();
+	}	
+}

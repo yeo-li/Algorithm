2dea2d177a793c38cd6e5dd8a29aaf067d693929
yeo-li <81564503+yeo-li@users.noreply.github.com>
2025-01-03 23:56:01 +0900

    [Bronze V] Title: Hook, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+	static int M;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("#  # #### #### #  #\n"
+				+ "#### #  # #  # # #\n"
+				+ "#### #  # #  # # #\n"
+				+ "#  # #### #### #  #");
+	}
+	
+
+}
---
+++ 백준/Bronze/10189. Hook/README.md
@@ -0,0 +1,28 @@
+# [Bronze V] Hook - 10189 
+
+[문제 링크](https://www.acmicpc.net/problem/10189) 
+
+### 성능 요약
+
+메모리: 14092 KB, 시간: 100 ms
+
+### 분류
+
+구현
+
+### 제출 일자
+
+2025년 1월 3일 23:55:51
+
+### 문제 설명
+
+<p>Print out the word Hook as shown below.</p>
+
+### 입력 
+
+ Empty
+
+### 출력 
+
+ <p>Print out the word Hook as shown below.</p>
+

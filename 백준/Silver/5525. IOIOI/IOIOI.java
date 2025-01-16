70c066cbcf4def573b8e51f5f4c315204b9c50e2
yeo-li <81564503+yeo-li@users.noreply.github.com>
2025-01-16 10:28:35 +0900

    [Silver I] Title: IOIOI, Time: 236 ms, Memory: 22044 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -7,31 +7,25 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int M = Integer.parseInt(br.readLine());
-		char[] input = br.readLine().toCharArray();
-		char[] Pn = createPn(N).toCharArray();
+		char[] S = br.readLine().toCharArray();
 		
 		int cnt = 0;
-		for(int i = 0; i < input.length - Pn.length + 1; i++) {
-			boolean isPn = true;
-			for(int j = 0; j < Pn.length; j++) {
-				if(input[i + j] != Pn[j]) {
-					isPn = false;
-					break;
+		int ans = 0;
+		for(int i = 0; i < M - 2;) {
+			if(S[i] == 'I' && S[i+1] == 'O' && S[i+2] == 'I') {
+				i+=2;
+				cnt++;
+				if(cnt == N) {
+					ans++;
+					cnt--;
 				}
+			} else {
+				i++;
+				cnt = 0;
 			}
-			
-			if(isPn) cnt++;
 		}
 		
-		System.out.println(cnt);
+		System.out.println(ans);
 	}
 	
-	public static String createPn(int N) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("I");
-		for(int i = 0; i < N; i++) 
-			sb.append("OI");
-		
-		return sb.toString();
-	}
 }

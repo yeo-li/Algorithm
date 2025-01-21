0df4c93823f871494ed7fa997074cf8d2848d81e
yeo-li <81564503+yeo-li@users.noreply.github.com>
2025-01-21 16:43:27 +0900

    [Gold III] Title: 아기 상어, Time: 184 ms, Memory: 24136 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -2,108 +2,101 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N, size = 2;
-	static int[][] sea, dis;
-	static int[] dy = { 1, -1, 0, 0 };
-	static int[] dx = { 0, 0, 1, -1 };
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		sea = new int[N][N];
-		dis = new int[N][N];
-		int y = 0, x = 0;
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				sea[i][j] = Integer.parseInt(st.nextToken());
-				if (sea[i][j] == 9) { // 상어 위치 찾기
-					sea[i][j] = 0;
-					y = i;
-					x = j;
+	static int n, sharkY, sharkX, sharkSize, fishCnt, time;
+	static int[][] board;
+	static boolean[][] visited;
+	static int[] dy = { -1, 1, 0, 0 };
+	static int[] dx = { 0, 0, -1, 1 };
+
+	public static boolean isValid(int y, int x) {
+		return y >= 0 && y < n && x >= 0 && x < n;
+	}
+	
+	public static boolean bfs() {
+		// int[] : y좌표, x좌표, 총 거리 저장
+		PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[2] == o2[2]) {
+					if (o1[0] == o2[0]) {
+						return o1[1] - o2[1];
+					}
+					return o1[0] - o2[0];
 				}
-			}
-		}
-
-		System.out.println(moveBabyShark(y, x));
-	}
-
-	public static int moveBabyShark(int y, int x) {
-		size = 2;
-		int time = 0;
-		int fishCnt = 0;
-		int startY = y;
-		int startX = x;
-		while (true) {
-			int[] now = bfs(startY, startX);
-			if (now == null)
-				break;
-
-			int ny = now[0];
-			int nx = now[1];
-			time += now[2];
-
-			sea[ny][nx] = 0;
-			fishCnt++;
-
-			if (fishCnt == size) {
-				fishCnt = 0;
-				size++;
-			}
-			
-			startY = now[0];
-			startX = now[1];
-		}
-		
-		
-
-		return time;
-	}
-
-	public static int[] bfs(int y, int x) {
-		boolean[][] visited = new boolean[N][N];
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
-			if (o1[2] != o2[2])
 				return o1[2] - o2[2];
-			if (o1[0] != o2[0])
-				return o1[0] - o2[0];
-			return o1[1] - o2[1];
+			}
 		});
 
 		Deque<int[]> dq = new ArrayDeque<>();
-		dq.offer(new int[] { y, x, 0 });
-		visited[y][x] = true;
+		dq.offer(new int[] { sharkY, sharkX, 0 });
+		visited = new boolean[n][n];
+		visited[sharkY][sharkX] = true;
+		
 		while (!dq.isEmpty()) {
 			int[] now = dq.poll();
-			for (int i = 0; i < 4; i++) {
-				int ny = now[0] + dy[i];
-				int nx = now[1] + dx[i];
+			int y = now[0];
+			int x = now[1];
+			int t = now[2];				
 
-				if (isPassable(ny, nx) && !visited[ny][nx]) {
+			for (int i = 0; i < 4; i++) {
+				int ny = y + dy[i];
+				int nx = x + dx[i];
+
+				if (isValid(ny, nx) && !visited[ny][nx] && board[ny][nx] <= sharkSize) {
 					visited[ny][nx] = true;
-					// 먹을 수 있는건 따로 담기
-					if (isEdible(ny, nx))
-						pq.offer(new int[] { ny, nx, now[2] + 1 });
-					// 이와 별개로 지나갈 수 있는 얘도 따로 담기
-					dq.offer(new int[] { ny, nx, now[2] + 1 });
+					dq.offer(new int[] { ny, nx, t + 1 });
+					if (board[ny][nx] != 0 && board[ny][nx] < sharkSize) {
+						pq.add(new int[] { ny, nx, t + 1 });
+					}
+				}
+			}
+			
+		}
+		
+		if (!pq.isEmpty()) {
+			int[] fish = pq.poll();
+			int fy = fish[0];
+			int fx = fish[1];
+			time += fish[2];
+			fishCnt++;
+			board[fy][fx] = 0;
+			sharkY = fy;
+			sharkX = fx;
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		board = new int[n][n];
+		sharkY = 0;
+		sharkX = 0;
+		StringTokenizer st;
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+				if (board[i][j] == 9) {
+					sharkY = i;
+					sharkX = j;
+					board[i][j] = 0;
 				}
 			}
 		}
-
-		if (pq.isEmpty())
-			return null;
-		return pq.poll();
-	}
-
-	public static boolean isValid(int y, int x) {
-		return 0 <= y && y < N && 0 <= x && x < N;
-	}
-
-	public static boolean isEdible(int y, int x) {
-		return isValid(y, x) && sea[y][x] < size && sea[y][x] != 0;
-	}
-
-	public static boolean isPassable(int y, int x) {
-		return isValid(y, x) && sea[y][x] <= size;
+		
+		sharkSize = 2;
+		time = 0;
+		fishCnt = 0;
+		while (bfs()) {
+			if (sharkSize == fishCnt) {
+				sharkSize++;
+				fishCnt = 0;
+			}
+		}
+		
+		System.out.println(time);
 	}
 }

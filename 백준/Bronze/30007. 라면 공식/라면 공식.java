a159c13036ff2a9a0648585df2200b3dafcc1cb3
yeo-li <81564503+yeo-li@users.noreply.github.com>
2025-01-28 22:32:23 +0900

    [Bronze V] Title: 라면 공식, Time: 104 ms, Memory: 14576 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		while (N-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int X = Integer.parseInt(st.nextToken());
+			sb.append(A*(X - 1) + B).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+}

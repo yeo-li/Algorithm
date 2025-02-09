59feac2cd1f04e15b9defec3bf6b6fb4f876fa8b
yeo-li <81564503+yeo-li@users.noreply.github.com>
2025-02-09 12:23:48 +0900

    [Bronze V] Title: 콜센터, Time: 104 ms, Memory: 13952 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("     /~\\\n"
+				+ "    ( oo|\n"
+				+ "    _\\=/_\n"
+				+ "   /  _  \\\n"
+				+ "  //|/.\\|\\\\\n"
+				+ " ||  \\ /  ||\n"
+				+ "============\n"
+				+ "|          |\n"
+				+ "|          |\n"
+				+ "|          |");
+	}
+}

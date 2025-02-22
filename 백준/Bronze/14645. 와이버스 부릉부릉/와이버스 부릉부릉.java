df201a9527b6e3cad3ffc2b164dff2bc484072ab
Seong-yeol, Park <81564503+yeo-li@users.noreply.github.com>
2025-02-22 16:56:11 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 100 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+		}
+		
+		System.out.println("비와이");
+	}
+
+}

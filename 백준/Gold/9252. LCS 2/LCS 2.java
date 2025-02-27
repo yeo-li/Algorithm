8263d79d255c63ff55540355a08ba395e3beab5d
Seong-yeol, Park <81564503+yeo-li@users.noreply.github.com>
2025-02-27 13:18:57 +0900

    [Gold IV] Title: LCS 2, Time: 128 ms, Memory: 18432 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,14 +1,11 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
 	static int[][] dp;
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char[] A = br.readLine().toCharArray();
 		char[] B = br.readLine().toCharArray();
-
 		dp = new int[A.length + 1][B.length + 1];
 		int[] idxArr = new int[A.length - 1];
 		for (int i = 1; i <= A.length; i++) {
@@ -18,27 +15,21 @@ public class Main {
 				} else {
 					dp[i][j] += Math.max(dp[i - 1][j], dp[i][j - 1]);
 				}
-//
-//				if (dp[i][j] > i)
-//					dp[i][j] = i;
-//				if (dp[i][j] > j)
-//					dp[i][j] = j;
 			}
 		}
-
 		StringBuilder sb = new StringBuilder();
 		sb.append(dp[A.length][B.length]);
-
 		if (dp[A.length][B.length] != 0) {
 			sb.append("\n");
-
-			String lcs = "";
+			char[] lcs = new char[dp[A.length][B.length]];
+			int idx = dp[A.length][B.length] - 1;
 			int i = A.length, j = B.length;
 			while (i > 0 && j > 0) {
 				if (A[i - 1] == B[j - 1]) {
-					lcs = A[i - 1] + lcs;
+					lcs[idx] = A[i - 1];
 					i--;
 					j--;
+					idx--;
 				} else {
 					if (dp[i - 1][j] > dp[i][j - 1]) {
 						i--;
@@ -47,10 +38,9 @@ public class Main {
 					}
 				}
 			}
-
-			sb.append(lcs);
+			for (char c : lcs)
+				sb.append(c);
 		}
-
 		System.out.println(sb);
 	}
 

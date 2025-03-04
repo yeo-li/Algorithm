5b1c9ce8204be70dd93e2a82bf0395c1adc35163
Seong-yeol, Park <81564503+yeo-li@users.noreply.github.com>
2025-03-04 12:52:39 +0900

    [Gold IV] Title: 여행 가자, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,66 +1,65 @@
 import java.io.*;
 
 public class Main {
-    static int[] parent, rank;
+	static int[] parent, rank;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) throws Exception {
-        int N = read();
-        int M = read();
+	public static void main(String[] args) throws Exception {
 
-        parent = new int[N + 1];
-        rank = new int[N + 1];
+		int N = read();
+		int M = read();
+		parent = new int[N + 1];
+		rank = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+			rank[i] = 1;
+		}
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (read() == 1)
+					union(i, j);
+			}
+		}
+		int firstCity = find(read());
+		for (int i = 1; i < M; i++) {
+			if (find(read()) != firstCity) {
+				bw.write("NO");
+				bw.flush();
+				bw.close();
+				return;
+			}
+		}
+		bw.write("YES");
+		bw.flush();
+		bw.close();
+	}
 
-        for (int i = 1; i <= N; i++) {
-            parent[i] = i;
-            rank[i] = 1;
-        }
+	public static void union(int x, int y) {
+		int rootX = find(x);
+		int rootY = find(y);
+		if (rootX != rootY) {
+			if (rank[rootX] > rank[rootY]) {
+				parent[rootY] = rootX;
+			} else if (rank[rootX] < rank[rootY]) {
+				parent[rootX] = rootY;
+			} else {
+				parent[rootY] = rootX;
+				rank[rootX]++;
+			}
+		}
+	}
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (read() == 1) {
-                    union(i, j);
-                }
-            }
-        }
+	public static int find(int x) {
+		if (parent[x] != x) {
+			parent[x] = find(parent[x]);
+		}
+		return parent[x];
+	}
 
-        int firstCity = find(read());
-        for (int i = 1; i < M; i++) {
-            if (find(read()) != firstCity) {
-                System.out.println("NO");
-                return;
-            }
-        }
-
-        System.out.println("YES");
-    }
-
-    public static void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if (rootX != rootY) {
-            if (rank[rootX] > rank[rootY]) {
-                parent[rootY] = rootX;
-            } else if (rank[rootX] < rank[rootY]) {  // 기존 코드에서 조건 오류 수정
-                parent[rootX] = rootY;
-            } else {
-                parent[rootY] = rootX;
-                rank[rootX]++;
-            }
-        }
-    }
-
-    public static int find(int x) {
-        if (parent[x] != x) {
-            return parent[x] = find(parent[x]);  // **경로 압축 적용**
-        }
-        return x;
-    }
-
-    private static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32)
-            n = (n << 3) + (n << 1) + (c & 15);
-        return n;
-    }
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
+	}
 }

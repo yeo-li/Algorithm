538c060689853aed83fb7750a6547e82342bc6a3
Seong-yeol, Park <81564503+yeo-li@users.noreply.github.com>
2025-03-04 12:44:50 +0900

    [Gold IV] Title: 여행 가자, Time: 112 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -2,42 +2,68 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] parent;
+	static int[] parent, rank;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		int N = read();
+		int M = read();
 
-        parent = new int[N + 1];
-        for (int i = 1; i <= N; i++) parent[i] = i;
+		parent = new int[N + 1];
+		rank = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+			rank[i] = 1;
+		}
 
-        // Union-Find로 도시 연결
-        for (int i = 1; i <= N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                if (Integer.parseInt(st.nextToken()) == 1) union(i, j);
-            }
-        }
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (read() == 1) {
+					union(i, j);
+				}
+			}
+		}
 
-        // 여행 계획 검증
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int firstCity = find(Integer.parseInt(st.nextToken())); // 첫 번째 도시의 루트
-        for (int i = 1; i < M; i++) {
-            if (find(Integer.parseInt(st.nextToken())) != firstCity) {
-                System.out.println("NO");
-                return;
-            }
-        }
-        System.out.println("YES");
-    }
+		int firstCity = find(read());
+		for (int i = 1; i < M; i++) {
+			if (find(read()) != firstCity) {
+				System.out.println("NO");
+				return;
+			}
 
-    public static void union(int x, int y) {
-        int rootX = find(x), rootY = find(y);
-        if (rootX != rootY) parent[rootY] = rootX;
-    }
+		}
 
-    public static int find(int x) {
-        return parent[x] = (parent[x] == x) ? x : find(parent[x]);
-    }
+		System.out.println("YES");
+
+	}
+
+	public static void union(int x, int y) {
+		int rootX = find(x);
+		int rootY = find(y);
+
+		if (rootX != rootY) {
+			if (rank[rootX] > rank[rootY]) {
+				parent[rootY] = rootX;
+			} else if (rank[rootX] > rank[rootY]) {
+				parent[rootX] = rootY;
+			} else {
+				parent[rootY] = rootX;
+				rank[rootX]++;
+			}
+		}
+	}
+
+	public static int find(int x) {
+		if (parent[x] != x) {
+			parent[x] = find(parent[x]);
+		}
+		return parent[x];
+	}
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
+	}
+
 }

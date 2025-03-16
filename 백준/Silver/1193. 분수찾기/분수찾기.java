3cbea2c4b8c2fa96f9820a59dbae888e4b25ee82
Seong-yeol, Park <81564503+yeo-li@users.noreply.github.com>
2025-03-16 20:46:51 +0900

    [Silver V] Title: 분수찾기, Time: 128 ms, Memory: 16016 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -8,9 +8,10 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 
-		int num = calculateXY(N);
-		// System.out.println(num);
-		
+		int num = 0;
+		for (num = 0; num * num / 2.0 + num / 2.0 < N; num++) {
+		}
+
 		int A = 1;
 		int B = num;
 		for(double i = num*num/2.0 + num/2.0; i > N; i--) {
@@ -18,28 +19,12 @@ public class Main {
 			B--;
 		}
 		
-		if(num%2 != 0) { // up
-			// up 이면 넓이의 가장 마지막숫자의 값을 알 수 있음
-			// 따라서 up은 마지막 수가 [0][num] 임 -> 이 뜻은 1/num 부터라는 뜻
-			
-			
-			
+		if(num%2 != 0) { 
 			System.out.println(A+"/"+B);
-			
-			
-		} else { // down
-			// num/1 부터 숫자가 줄어듦
-			
+		} else { 
 			System.out.println(B+"/"+A);
 			
 		}
 	}
 
-	public static int calculateXY(int N) {
-		int i = 0;
-		for (i = 0; i * i / 2.0 + i / 2.0 < N; i++) {
-		}
-
-		return i;
-	}
 }

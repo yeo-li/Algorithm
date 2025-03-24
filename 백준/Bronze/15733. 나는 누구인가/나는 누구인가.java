6997eb0a63e036ce1d48d283c9889b06f990fd29
Seong-yeol, Park <81564503+yeo-li@users.noreply.github.com>
2025-03-24 21:35:51 +0900

    [Bronze V] Title: 나는 누구인가, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static boolean[][] board;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+//		int N = Integer.parseInt(st.nextToken());
+//		int M = Integer.parseInt(st.nextToken());
+//
+//		board = new boolean[N][M];
+//
+//		for (int i = 0; i < N; i++) {
+//			st = new StringTokenizer(br.readLine());
+//			for (int j = 0; j < M; j++) {
+//				if (st.nextToken().equals("1"))
+//					board[i][j] = true;
+//			}
+//		}
+//		
+
+		System.out.println("I'm Sexy");
+	}
+
+}

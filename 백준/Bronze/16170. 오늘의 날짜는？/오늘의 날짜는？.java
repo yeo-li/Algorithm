b14bd975bc476e3cbd2d0bd83f8bb798b873cef6
Seong-yeol, Park <81564503+yeo-li@users.noreply.github.com>
2025-03-28 21:36:13 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 104 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("2025\n03\n28");
+	}
+
+}

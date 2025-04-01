16381620ee27224b0d8acf758115fde0113d0362
Seong-yeol, Park <81564503+yeo-li@users.noreply.github.com>
2025-04-01 23:49:10 +0900

    [Bronze V] Title: 새로운 시작, Time: 100 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("파이팅!!");
+	}
+
+}

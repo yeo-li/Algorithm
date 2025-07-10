ab1144a2f9a7cb5fbd419aea4e239e2ecc4d4fe1
Seong-yeol, Park <81564503+yeo-li@users.noreply.github.com>
2025-07-10 23:56:41 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int[] T;
+	static int[] P;
+	static int[] dp;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		T = new int[N];
+		P = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+
+		dp = new int[N + 1];
+
+		for (int i = 0; i < N; i++) {
+			if (i + T[i] <= N) {
+				dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]);
+			}
+			dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+		}
+
+		System.out.println(dp[N]);
+	}
+
+}

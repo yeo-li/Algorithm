771f0216c14770f3c6cb0b04a55bdb4e2ed9f052
Park, Seong-yeol <81564503+yeo-li@users.noreply.github.com>
2025-08-10 00:25:30 +0900

    [Silver I] Title: 나이트의 이동, Time: 268 ms, Memory: 49860 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
 	static int I;
-	static int[][] board;
+	static boolean[][] board;
 	static int[] start = new int[2];
 	static int[] end = new int[2];
 	static int[][] dyx = { { 2, -1 }, { 2, 1 }, { 1, 2 }, { 1, -2 }, { -1, 2 }, { -1, -2 }, { -2, -1 }, { -2, 1 } };
@@ -17,12 +17,12 @@ public class Main {
 		for (int r = 0; r < N; r++) {
 			I = Integer.parseInt(br.readLine());
 
-			board = new int[I][I];
-			for (int i = 0; i < I; i++) {
-				for (int j = 0; j < I; j++) {
-					board[i][j] = Integer.MAX_VALUE;
-				}
-			}
+			board = new boolean[I][I];
+//			for (int i = 0; i < I; i++) {
+//				for (int j = 0; j < I; j++) {
+//					board[i][j] = Integer.MAX_VALUE;
+//				}
+//			}
 
 			st = new StringTokenizer(br.readLine());
 			start[0] = Integer.parseInt(st.nextToken());
@@ -41,8 +41,12 @@ public class Main {
 	public static int bfs() {
 		Deque<int[]> dq = new ArrayDeque<>();
 
-		dq.offer(new int[] { start[0], start[1], 0 }); // y, x, depth
-		board[start[0]][start[1]] = 0;
+		if (start[0] == end[0] && start[1] == end[1]) {
+			return 0;
+		}
+
+		dq.offer(new int[] { start[0], start[1], 0 });
+		board[start[0]][start[1]] = true;
 
 		while (!dq.isEmpty()) {
 			int[] xy = dq.poll();
@@ -50,14 +54,19 @@ public class Main {
 				int ny = xy[0] + dyx[i][0];
 				int nx = xy[1] + dyx[i][1];
 
-				if (isPossable(ny, nx) && board[ny][nx] > (xy[2] + 1)) {
+				if (isPossable(ny, nx) && !board[ny][nx]) {
+
+					if (ny == end[0] && nx == end[1]) {
+						return xy[2] + 1;
+					}
+
 					dq.offer(new int[] { ny, nx, xy[2] + 1 });
-					board[ny][nx] = xy[2] + 1;
+					board[ny][nx] = true;
 				}
 			}
 		}
 
-		return board[end[0]][end[1]];
+		return -1;
 	}
 
 	public static boolean isPossable(int y, int x) {

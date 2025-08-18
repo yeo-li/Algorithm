7e9ef26ee16cd4b6a33fbaa8b2fc4485f1a6cf68
Park, Seong-yeol <81564503+yeo-li@users.noreply.github.com>
2025-08-18 22:37:16 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18064 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,33 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
 	static int[] dp;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
-		dp = new int[N+1];
-		
-		bw.write(String.valueOf(recursive(N)));
-		bw.close();
-	}
-	
-	private static int recursive(int N) {
-		if(N == 1) return 0;
-		
-		if(dp[N] == 0) {
-			if(N % 6 == 0) {
-				dp[N] = Math.min(recursive(N - 1), Math.min(recursive(N/3), recursive(N/2))) + 1;
-			} else if(N%3 == 0) {
-				dp[N] = Math.min(recursive(N/3), recursive(N-1)) + 1;
-			} else if(N % 2 == 0) {
-				dp[N] = Math.min(recursive(N/2), recursive(N-1)) + 1;
-			} else {
-				dp[N] = recursive(N-1) + 1;
+
+		dp = new int[N + 1];
+
+		dp[1] = 0;
+		for (int i = 2; i <= N; i++) {
+			dp[i] = Integer.MAX_VALUE;
+
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+			}
+
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+			}
+
+			if (i > 0) {
+				dp[i] = Math.min(dp[i - 1] + 1, dp[i]);
 			}
 		}
-		
-		return dp[N];
+
+		System.out.println(dp[N]);
 	}
+
 }

e29155c9961ef9f043f8cc051732f3dc2381ae33
Park, Seong-yeol <81564503+yeo-li@users.noreply.github.com>
2025-08-19 22:29:54 +0900

    [Gold V] Title: LCS, Time: 120 ms, Memory: 18248 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -2,26 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	static int[][] dp;
+	static char[] s1, s2;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		char[] A = br.readLine().toCharArray();
-		char[] B = br.readLine().toCharArray();
-		int[][] dp = new int[A.length + 1][B.length + 1];
 
-		int max = Integer.MIN_VALUE;
-		for (int i = 1; i <= A.length; i++) {
-			for (int j = 1; j <= B.length; j++) {
-				if (A[i - 1] == B[j - 1])
+		s1 = br.readLine().toCharArray();
+		s2 = br.readLine().toCharArray();
+
+		dp = new int[s1.length + 1][s2.length + 1];
+
+		for (int i = 1; i <= s1.length; i++) {
+			for (int j = 1; j <= s2.length; j++) {
+				if (s1[i - 1] == s2[j - 1])
 					dp[i][j] = dp[i - 1][j - 1] + 1;
 				else
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-
-				if (max < dp[i][j])
-					max = dp[i][j];
+					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
 			}
 		}
 
-		System.out.println(max);
+		System.out.println(dp[s1.length][s2.length]);
+
 	}
 
 }

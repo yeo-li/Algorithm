a7b55c7c172035bb54a198abd5eb5f6775c611cf
Park, Seong-yeol <81564503+yeo-li@users.noreply.github.com>
2025-08-25 03:54:57 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 96 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int M = Integer.parseInt(st.nextToken());
+			int F = Integer.parseInt(st.nextToken());
+
+			if (M == 0 && F == 0)
+				break;
+
+			sb.append(M + F).append("\n");
+
+		}
+
+		System.out.println(sb);
+	}
+
+}

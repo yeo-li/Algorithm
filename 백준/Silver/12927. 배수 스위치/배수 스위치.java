3043a616e207d389a98b458a2e4ef1ddbe1e83d3
Park, Seong-yeol <81564503+yeo-li@users.noreply.github.com>
2025-10-21 03:37:08 +0900

    [Silver IV] Title: 배수 스위치, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/12927. 배수 스위치/배수 스위치.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] bulbs = br.readLine().trim().toCharArray();
+        int n = bulbs.length;
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (bulbs[i] == 'Y') {
+                cnt++;
+                // i의 배수 전구 반전
+                for (int j = i; j < n; j += (i + 1)) {
+                    bulbs[j] = (bulbs[j] == 'Y') ? 'N' : 'Y';
+                }
+            }
+        }
+
+        // 결과 확인
+        for (char c : bulbs) {
+            if (c == 'Y') {
+                System.out.println(-1);
+                return;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}

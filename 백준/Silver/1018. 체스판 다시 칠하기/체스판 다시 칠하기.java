de072d8cf32e0df201d6b3b4ce5f20aa55f5ebe7
Park, Seong-yeol <81564503+yeo-li@users.noreply.github.com>
2026-03-16 17:43:40 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 112 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static char[][] board;
+	static int N, M;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		board = new char[N][M];
+		for (int i = 0; i < N; i++) {
+			char[] input = br.readLine().toCharArray();
+			for (int j = 0; j < M; j++)
+				board[i][j] = input[j];
+		}
+
+		int cnt = Integer.MAX_VALUE;
+		for (int i = 0; i <= N - 8; i++) {
+			for (int j = 0; j <= M - 8; j++) {
+				cnt = Math.min(cnt, chess(i, j, i + 8, j + 8, 'W', 'B'));
+				cnt = Math.min(cnt, chess(i, j, i + 8, j + 8, 'B', 'W'));
+			}
+		}
+
+		if (cnt == Integer.MAX_VALUE)
+			cnt = 0;
+
+		System.out.println(cnt);
+
+	}
+
+	public static int chess(int startY, int startX, int endY, int endX, char c1, char c2) {
+		int cnt = 0;
+		for (int i = startY; i < endY; i++) {
+			char c = c1;
+			if (i % 2 != 0)
+				c = c2;
+
+			for (int j = startX; j < endX; j++) {
+				if (board[i][j] != c)
+					cnt++;
+				c = c1 == c ? c2 : c1;
+			}
+		}
+
+		return cnt;
+	}
+
+}

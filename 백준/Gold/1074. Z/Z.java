9323e0b44626f989b6c999997b7dd8f3cf9c4377
Park, Seong-yeol <81564503+yeo-li@users.noreply.github.com>
2026-04-05 21:49:21 +0900

    [Gold V] Title: Z, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -2,40 +2,39 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static long R;
-	static long C;
-	static long cnt = 0;
-	public static void main(String[] args) throws Exception {
+	static int r, c;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		int N = Integer.parseInt(st.nextToken());
-		R = Long.parseLong(st.nextToken());
-		C = Long.parseLong(st.nextToken());
-		
-		long k = 1L << N;
-		squareZ(0, 0, k);
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+
+		dfs(0, N, 0, 0);
+
+		System.out.println();
 	}
-	
-	private static void squareZ(long r, long c, long N) {
-		if(N == 1) {
-			System.out.println(cnt);
+
+	public static void dfs(int w, int n, int x, int y) {
+		if (!(y <= r && r < y + (int) Math.pow(2, n) && x <= c && c < x + (int) Math.pow(2, n))) {
 			return;
 		}
-		
-		boolean isLeft = C < c + N/2;
-		boolean isUp = R < r + N/2;
-		
-		if(isLeft && isUp) { // 1사 분면이라면
-			squareZ(r, c, N/2);
-		} else if(!isLeft && isUp) { // 2사 분면이라면
-			cnt += N/2*N/2;
-			squareZ(r, c + N/2, N/2);
-		} else if(isLeft && !isUp) { // 3사 분면 이라면
-			cnt += N/2*N/2*2;
-			squareZ(r + N/2, c, N/2);
-		} else {
-			cnt += N/2*N/2*3;
-			squareZ(r + N/2, c + N/2, N/2);
+
+		if (y == r && x == c) {
+			System.out.println(w);
+			return;
 		}
+
+		int sq = (int) Math.pow(2, n - 1);
+
+		dfs(w, n - 1, x, y);
+		dfs(w + sq * sq, n - 1, x + sq, y);
+		dfs(w + 2 * sq * sq, n - 1, x, y + sq);
+		dfs(w + 3 * sq * sq, n - 1, x + sq, y + sq);
+
+		return;
 	}
 }

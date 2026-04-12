919dd4991e92d8ca15f1ac336f1008a7719de7ec
Park, Seong-yeol <81564503+yeo-li@users.noreply.github.com>
2026-04-13 08:57:55 +0900

    [Gold IV] Title: N-Queen, Time: 6324 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -2,111 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int[][] board;
-	static int N, cnt = 0;
+	static int N;
+	static int[] col;
+	static int cnt = 0;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		N = Integer.parseInt(br.readLine());
-		board = new int[N][N];
-		
-		dfs(0, 0);
-		
+		col = new int[N];
+
+		dfs(0);
+
 		System.out.println(cnt);
 	}
 
-	public static void dfs(int depth, int row) {
-		if (depth == N) {
+	public static void dfs(int row) {
+		if (row == N) {
 			cnt++;
 			return;
 		}
+		for (int c = 0; c < N; c++) {
+			col[row] = c;
 
-
-		for (int p = 0; p < N; p++) {
-			int y = row;
-			int x = p;
-
-			if (board[y][x] == 0) {
-				for (int i = 0; i < N; i++) {
-					board[i][x]++;
-					board[y][i]++;
-				}
-
-				int ny = y;
-				int nx = x;
-				while(ny >= 0 && nx >= 0) {
-					board[ny][nx]++;
-					ny--;
-					nx--;
-				}
-				
-				ny = y;
-				nx = x;
-				while(ny < N && nx < N) {
-					board[ny][nx]++;
-					ny++;
-					nx++;
-				}
-				
-				ny = y;
-				nx = x;
-				while(ny >= 0 && nx < N) {
-					board[ny][nx]++;
-					ny--;
-					nx++;
-				}
-				
-				ny = y;
-				nx = x;
-				while(ny < N && nx >= 0) {
-					board[ny][nx]++;
-					ny++;
-					nx--;
-				}
-				
-				dfs(depth+1, row + 1);
-				
-				
-				// 원상 복귀
-				for (int i = 0; i < N; i++) {
-					board[i][x]--;
-					board[y][i]--;
-				}
-
-				ny = y;
-				nx = x;
-				while(ny >= 0 && nx >= 0) {
-					board[ny][nx]--;
-					ny--;
-					nx--;
-				}
-				
-				ny = y;
-				nx = x;
-				while(ny < N && nx < N) {
-					board[ny][nx]--;
-					ny++;
-					nx++;
-				}
-				
-				ny = y;
-				nx = x;
-				while(ny >= 0 && nx < N) {
-					board[ny][nx]--;
-					ny--;
-					nx++;
-				}
-				
-				ny = y;
-				nx = x;
-				while(ny < N && nx >= 0) {
-					board[ny][nx]--;
-					ny++;
-					nx--;
-				}
+			if (isPossible(row)) {
+				dfs(row + 1);
 			}
 		}
+	}
+
+	public static boolean isPossible(int row) {
+		for (int i = 0; i < row; i++) {
+			if (col[i] == col[row]) {
+				return false;
+			}
+
+			if (Math.abs(row - i) == Math.abs(col[row] - col[i])) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }

9820e9a8c88a68d56712e937b0445b30f377a6d3
Park, Seong-yeol <81564503+yeo-li@users.noreply.github.com>
2026-09-22 20:11:48 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        
+        
+        int[] diff = new int[progresses.length];
+        for(int i = 0; i < progresses.length; i++) {
+            diff[i] = (100 - progresses[i]) / speeds[i];
+            diff[i] += (100 - progresses[i]) % speeds[i] == 0? 0 : 1;
+        }
+        
+        List<Integer> ans = new ArrayList<>();
+        int cnt = 1;
+        int number = diff[0];
+        boolean isEnd = true;
+        for(int i = 1; i < progresses.length; i++) {
+            if(i == progresses.length) break;
+            if(number >= diff[i]) {
+                cnt++;
+                isEnd = false;
+            } else {
+                ans.add(cnt);
+                cnt = 1;
+                number = diff[i];
+                isEnd = true;
+            }
+        }
+        
+        ans.add(cnt);
+        
+        
+        int[] answer = new int[ans.size()];
+        for(int i = 0; i < ans.size(); i++) {
+            answer[i] = ans.get(i);
+        }
+        
+        return answer;
+    }
+    
+}
